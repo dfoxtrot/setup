09b7a0b4eb130282019a5feea26606ec29503cd1
dfoxtrot <david.frezzato@gmail.com>
2013-09-22 15:22:28 +0000

    ajout installation unzip

--- setup.sh
+++ setup.sh
@@ -27,6 +27,9 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
+# Install unzip
+sudo apt-get install unzip
+
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
